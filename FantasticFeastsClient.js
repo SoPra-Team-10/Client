77b72a24d43a28d1a3ce203ae11882f210d55ea2
Tarik Enderes <tarik.enderes@uni-ulm.de>
2019-04-10 18:08:20 +0200

    Basic HTML structure, defined functions to switch between views

---
+++ FantasticFeastsClient.js
@@ -0,0 +1,70 @@
+function toMainMenu(){
+    document.getElementById("MainMenu").style.display="initial";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toMatchmaking(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="initial";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toHelp(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="initial";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toHotkeys(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="initial";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toTeamConfiguration(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="initial";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toGameEnd(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="initial";
+    document.getElementById("InGame").style.display="none";
+}
+
+function toInGame(){
+    document.getElementById("MainMenu").style.display="none";
+    document.getElementById("Matchmaking").style.display="none";
+    document.getElementById("Help").style.display="none";
+    document.getElementById("Hotkeys").style.display="none";
+    document.getElementById("TeamConfiguration").style.display="none";
+    document.getElementById("GameEnd").style.display="none";
+    document.getElementById("InGame").style.display="initial";
+}
+
