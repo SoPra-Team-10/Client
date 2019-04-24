e0ad7ed9bdb47c78af0dd25d8de3cdf7a9218faa
Michael von Hohnhorst <michael.schmidt-von-schwind@uni-ulm.de>
2019-04-24 15:37:05 +0200

    test

--- fantastic-feasts/src/App.vue
+++ fantastic-feasts/src/App.vue
@@ -1,40 +1,41 @@
 <template>
   <div>
-    <app-menu v-if="gameState.inMenu"></app-menu>
+    <h1>Hello World</h1>
+    <!-- <app-menu v-if="gameState.inMenu"></app-menu>
     <app-lobby v-if="gameState.inLobby"></app-lobby>
     <app-help v-if="gameState.inHelp"></app-help>
     <app-team v-if="gameState.inTeam"></app-team>
-    <app-game v-if="gameState.inGame"></app-game>
-    
+    <app-game v-if="gameState.inGame"></app-game> -->
+
   </div>
 </template>
 
 <script>
-import Game from './components/Game.vue';
-import Help from './components/Help.vue';
-import Lobby from './components/Lobby.vue';
-import Team from './components/Team.vue';
-import Menu from './components/Menu.vue';
+// import Game from './components/Game.vue';
+// import Help from './components/Help.vue';
+// import Lobby from './components/Lobby.vue';
+// import Team from './components/Team.vue';
+// import Menu from './components/Menu.vue';
 
 export default {
   name: 'app',
   data () {
     return {
       gameState: {
-        inMenu: true,
-        inLobby: false,
-        inHelp: false,
-        inTeam: false,
-        inGame: false
+        // inMenu: true,
+        // inLobby: false,
+        // inHelp: false,
+        // inTeam: false,
+        // inGame: false
       }
     }
   },
   components: {
-    'app-game': Game,
-    'app-menu': Menu,
-    'app-help': Help,
-    'app-lobby': Lobby,
-    'app-team': Team
+    // 'app-game': Game,
+    // 'app-menu': Menu,
+    // 'app-help': Help,
+    // 'app-lobby': Lobby,
+    // 'app-team': Team
   }
   
 }
