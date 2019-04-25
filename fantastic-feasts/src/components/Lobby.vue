74a6fa3f9ebd3fa91660d4f28df15763238e4d37
Tarik <tarik.enderes@uni-ulm.de>
2019-04-25 12:13:51 +0200

    Websocket in Lobby

--- fantastic-feasts/src/components/Lobby.vue
+++ fantastic-feasts/src/components/Lobby.vue
@@ -3,10 +3,10 @@
         <h1>Lobby</h1>
         <p>Hier ist die Lobby</p>
         <label for="port">Port</label>
-        <input id="port" type="text">
+        <input id="port" type="text" v-model="port">
         <label for="server">Server</label>
-        <input id="server" type="text">
-        <button>Connect</button>
+        <input id="server" type="text" v-model="server">
+        <button v-on:click="connect()">Connect</button>
         <hr>
         <div class="row">
             <button @click="game.currentState = 'inMenu'">Zurück zum Menu</button>
@@ -16,7 +16,18 @@
 
 <script>
 export default {
+    methods: {
+        connect : function () {
+            var server = document.getElementById("server").value;
+            var port = document.getElementById("port").value;
+            var websocket = new WebSocket(server, ['soap', 'xmpp']);
+            websocket.onerror = function (error) {
+                console.log('WebSocket Error ' + error);
+            };
+        }
+    },
     props: ['game']
+    
 }
 </script>
 
