7275826f05948d9ae6e8c0fb187d45fe0ab398ba
Michael von Hohnhorst <michael.schmidt-von-schwind@uni-ulm.de>
2019-04-27 16:14:56 +0200

    Fixed button issue

--- fantastic-feasts/src/components/Team.vue
+++ fantastic-feasts/src/components/Team.vue
@@ -2,8 +2,10 @@
     <div>
         <h1 class="subtitle">Team auswählen</h1>
         <button class="main-menu__button">Dateien</button>
-        <hr>
-        <button @click="game.currentState = 'inMenu'" class="main-menu__button footer-bar">Zurück zum Menü</button>
+        <div class="main-menu__button-container footer-bar">
+            <hr>
+            <button @click="game.currentState = 'inMenu'" class="main-menu__button">Zurück zum Menü</button>
+        </div>
     </div>
 </template>
 
